5c60b567f5ded580211b9305e4715768b1ce9745
Shantanu Jain <shjain@codeaurora.org>
2013-12-24 16:59:41 +0530

    input: touchscreen: request threaded-only IRQs with IRQF_ONESHOT
    Threaded IRQs without primary handler need to be requested with
    IRQF_ONESHOT, otherwise the request will fail. This patch adds the
    IRQF_ONESHOT to the focaltech touchscreen driver where it is
    missing.
    
    Change-Id: I429f48126bd5d28c6dbf6ba846175d5e643dd436
    Signed-off-by: Shantanu Jain <shjain@codeaurora.org>

--- drivers/input/touchscreen/ft5x06_ts.c
+++ drivers/input/touchscreen/ft5x06_ts.c
@@ -1540,8 +1540,9 @@ static int ft5x06_ts_probe(struct i2c_client *client,
 	data->family_id = reg_value;
 
 	err = request_threaded_irq(client->irq, NULL,
-				   ft5x06_ts_interrupt, pdata->irqflags,
-				   client->dev.driver->name, data);
+				ft5x06_ts_interrupt,
+				pdata->irqflags | IRQF_ONESHOT,
+				client->dev.driver->name, data);
 	if (err) {
 		dev_err(&client->dev, "request irq failed\n");
 		goto free_reset_gpio;
